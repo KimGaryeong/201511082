7eb2f8c43961c11547181099dbf7bd7b3c5af1fa
KimGaryeong <mdok5403@naver.com>
2017-04-03 22:59:25 +0900

    Add files via upload

--- src/ds_web_crawl_python.py
+++ src/ds_web_crawl_python.py
@@ -3,7 +3,7 @@ import urllib
 uResponse = urllib.urlopen('http://python.org/')
 _html = uResponse.read()
 
-p=re.compile('href="(http://.*?)"')
+p=re.compile('<a\s*.*?>(.*?)</a>')
 nodes=p.findall(_html)
 for i, node in enumerate(nodes):
     print i, node
