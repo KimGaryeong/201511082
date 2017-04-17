39d6342fd6c671171f276517e3279102fb74af19
KimGaryeong <mdok5403@naver.com>
2017-04-17 14:07:16 +0900

    Add files via upload

---
+++ src/ds_save_mongo_6.py
@@ -0,0 +1,13 @@
+﻿# coding: utf-8
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient('localhost:27017')
+db = client.myDB
+db.myCol.insert_one({
+        "Persons": [{"id": "201511082", "이름": "김가령"},
+                    {"id": "201511082", "이름": "가령"}]
+    })
+results = db.myCol.find()
+for r in results:
+    print r['Persons']
